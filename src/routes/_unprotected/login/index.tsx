c9138d5bcf60bb5c84a6b6076875b0a774f3129d
raves21 <johnraves23@gmail.com>
2025-09-13 11:46:31 +0800

    chore: login form with reacthookform + shadcn

--- src/routes/_unprotected/login/index.tsx
+++ src/routes/_unprotected/login/index.tsx
@@ -1,64 +1,98 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Loader } from "lucide-react";
-import { useState } from "react";
 import { useLogin } from "../../../domains/auth/api/mutations";
-import { InputWithLabel } from "@/components/ui/input-with-label";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export const Route = createFileRoute("/_unprotected/login/")({
   component: RouteComponent,
 });
 
-function RouteComponent() {
-  const {
-    mutateAsync: login,
-    status: loginStatus,
-    error: loginError,
-  } = useLogin();
+const formSchema = z.object({
+  email: z.email({ error: "Email is badly formatted." }),
+  password: z.string().min(1, { error: "This field is required." }),
+});
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+function RouteComponent() {
+  const { mutateAsync: login, status: loginStatus } = useLogin();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit({ email, password }: z.infer<typeof formSchema>) {
+    await login({ email, password });
+  }
 
   return (
-    <div className="min-h-dvh w-dvw max-w-full relative flex items-center bg-gray-bg justify-center">
-      <div className="flex items-center justify-center gap-2 absolute top-6 left-6">
-        <Loader className="size-11 stroke-mainaccent" />
-        <p className="text-3xl font-semibold">CentraLearn</p>
-      </div>
-      <div className="flex flex-col gap-10 items-center p-24 shadow-md bg-main-bg rounded-md">
-        <p className="text-3xl font-semibold">Login</p>
-        <form
-          className="flex flex-col gap-5"
-          onSubmit={async (e) => {
-            e.preventDefault();
-            await login({ email, password });
-          }}
-        >
-          <InputWithLabel
-            label="Email"
-            value={email}
-            onChange={(e) => setEmail(e.currentTarget.value)}
-            containerClassName="w-[340px]"
-            type="text"
-          />
-          <InputWithLabel
-            label="Password"
-            value={password}
-            onChange={(e) => setPassword(e.currentTarget.value)}
-            containerClassName="w-[340px]"
-            type="text"
-          />
-          <button
-            disabled={loginStatus === "pending"}
-            className="flex hover:cursor-pointer disabled:hover:cursor-auto items-center disabled:bg-mainaccent-disabled justify-center gap-4 w-[340px] py-3 text-white rounded-md bg-mainaccent"
+    <div className="relative flex items-center justify-center max-w-full min-h-dvh w-dvw bg-main-bg">
+      <div className="flex flex-col items-center gap-10  w-[400px] rounded-md">
+        <div className="flex items-center justify-center gap-2">
+          <Loader className="size-11 stroke-mainaccent" />
+          <p className="text-3xl font-semibold">CentraLearn</p>
+        </div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col w-full gap-8"
           >
-            {loginStatus === "pending" ? (
-              <Loader className="animate-spin" />
-            ) : (
-              <p>Login</p>
-            )}
-          </button>
-          {loginStatus === "error" && <p>{loginError.message}</p>}
-        </form>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>
+                    Email <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormMessage />
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>
+                    Password <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormMessage />
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <button
+              type="submit"
+              disabled={loginStatus === "pending"}
+              className="flex items-center justify-center w-full gap-4 py-3 text-white rounded-md hover:cursor-pointer disabled:hover:cursor-auto disabled:bg-mainaccent-disabled bg-mainaccent"
+            >
+              {loginStatus === "pending" ? (
+                <Loader className="animate-spin" />
+              ) : (
+                <p>Login</p>
+              )}
+            </button>
+          </form>
+        </Form>
       </div>
     </div>
   );
